1116dbe7b2779f712c5476096ad1ff21226a4076
Raghu Betina <raghu@firstdraft.com>
2018-05-31 14:00:33 +0000

    Direct association between cities and neighborhoods

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,6 +1,9 @@
 class Neighborhood < ApplicationRecord
   # Direct associations
 
+  belongs_to :city,
+             :counter_cache => true
+
   has_many   :restaurants
 
   # Indirect associations
---
+++ db/migrate/20180531140033_add_neighborhood_count_to_cities.rb
@@ -0,0 +1,5 @@
+class AddNeighborhoodCountToCities < ActiveRecord::Migration[5.0]
+  def change
+    add_column :cities, :neighborhoods_count, :integer
+  end
+end
