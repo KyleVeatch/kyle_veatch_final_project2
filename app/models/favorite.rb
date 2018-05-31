1bd6216d1b44991264b874011d7c1433a79de472
Raghu Betina <raghu@firstdraft.com>
2018-05-31 14:00:38 +0000

    Direct association between users and favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,9 @@
 class Favorite < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :counter_cache => :bookmarks_count
+
   belongs_to :restaurant,
              :counter_cache => :bookmarks_count
 
